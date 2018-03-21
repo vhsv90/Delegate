bd7fce384dc5093bc169ad69213985d429a0fcda
Victor.SanchezValverde <Victor.SanchezValverde@qpharmacorp.com>
2018-03-21 08:26:21 -0600

    Math class with delegate added

---
+++ Delegate/Delegate/Math.cs
@@ -0,0 +1,53 @@
+﻿namespace Delegate
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Math
+    {
+
+        public delegate void PointToMathsOperation(int numberOne, int numberTwo);
+
+        PointToMathsOperation pointToMathsOperation = null;
+
+        public PointToMathsOperation Operation(string operationName)
+        {
+            switch (operationName.ToLower())
+            {
+                case "add":
+                    pointToMathsOperation = Add;
+                    return pointToMathsOperation;
+                case "sub":
+                    pointToMathsOperation = Sub;
+                    return pointToMathsOperation;
+                case "div":
+                    pointToMathsOperation = Mul;
+                    return pointToMathsOperation;
+                case "mul":
+                    pointToMathsOperation = Div;
+                    return pointToMathsOperation;
+            }
+            return pointToMathsOperation;
+        }
+
+        private void Add(int numberOne, int numberTwo)
+        {
+            Console.WriteLine((numberOne + numberTwo).ToString());
+        }
+        private void Sub(int numberOne, int numberTwo)
+        {
+            Console.WriteLine((numberOne - numberTwo).ToString());
+        }
+        private void Div(int numberOne, int numberTwo)
+        {
+            Console.WriteLine((numberOne / numberTwo).ToString());
+        }
+        private void Mul(int numberOne, int numberTwo)
+        {
+            Console.WriteLine((numberOne * numberTwo).ToString());
+        }
+
+    }
+
+}
--- Delegate/Delegate/Program.cs
+++ Delegate/Delegate/Program.cs
@@ -6,8 +6,19 @@ namespace Delegate
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Please enter two number:");
+            var numberOneString = Console.ReadLine();
+            var numberTwoString = Console.ReadLine();
+            Console.WriteLine("Please enter operation to perform like add,mul,sub and div :-");
+            var choice = Console.ReadLine();
+            int numberOne, numberTwo;
+            Math math = new Math();
+            int.TryParse(numberOneString, out numberOne);
+            int.TryParse(numberTwoString, out numberTwo);
+            math.Operation(choice).Invoke(numberOne, numberTwo);
             Console.ReadLine();
+
         }
     }
 }
